f708215c07fe29936656a9d8fe6a7ab72fc82b09
Thijs van Hoof <thijs.v.h@gmail.com>
2019-03-27 12:23:38 -0400

    Update camera.js

--- scripts/app/webgl/core/camera.js
+++ scripts/app/webgl/core/camera.js
@@ -2,19 +2,28 @@ define([
 	'backbone-webgl',
 	'three',
 	'model/display-model',
+	'model/state-model',
 	'util/orbit-control'
-], function (WebGL, THREE, DisplayModel, OrbitControl) {
+], function (WebGL, THREE, DisplayModel, StateModel, OrbitControl) {
 
 	return WebGL.extend({
 
 		initialize: function () {
 			var aspect = DisplayModel.get('aspect');
+			var depth = StateModel.get('depth');
 
 			this.camera = new THREE.PerspectiveCamera(35, aspect, 1, 5000);
-			this.camera.position.set(0, 0, 500);
-			this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+			this.camera.position.set(0, 200, 200);
 
-			this.control = new OrbitControl(this.camera);
+			this.target = new THREE.Vector3(0, 0, depth * -0.5);
+			this.control = new OrbitControl(this.camera, this.target, {
+				minDistance: 200,
+				maxDistance: 500,
+				minPolarAngle: 0.2 * Math.PI,
+				maxPolarAngle: 0.4 * Math.PI,
+				minAzimuthAngle: -0.15 * Math.PI,
+				maxAzimuthAngle: 0.15 * Math.PI
+			});
 
 			this.listenTo(DisplayModel, 'resize', this.onResize);
 		},
