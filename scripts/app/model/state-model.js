b849429c9eac8e2cd5b0b64bfb92a1aac3ce285c
Thijs van Hoof <thijs.v.h@gmail.com>
2019-03-27 18:21:00 -0400

    fog animation

--- scripts/app/model/state-model.js
+++ scripts/app/model/state-model.js
@@ -1,16 +1,54 @@
 define([
 	'backbone',
-	'three'
-], function (Backbone, THREE) {
+	'three',
+	'TweenLite'
+], function (Backbone, THREE, TweenLite) {
 
 	var StateModel = Backbone.Model.extend({
 
 		defaults: {
 			width: 800,
 			depth: 400,
-			fogColor: new THREE.Color(0x000000),
+			fogProps: null,
+			fogColor: new THREE.Color(),
 			fogNear: 200,
 			fogFar: 500
+		},
+
+		initialize: function () {
+			this.set({ fogProps: this.getNewFogProps() });
+			this.updateFogColor();
+		},
+
+		startTransition: function () {
+			var fogProps = this.get('fogProps');
+			var newFogProps = this.getNewFogProps();
+			if (newFogProps.h - fogProps.h > 0.5)
+				fogProps.h += 1;
+			if (fogProps.h - newFogProps.h > 0.5)
+				fogProps.h -= 1;
+			TweenLite.to(fogProps, 3, {
+				h: newFogProps.h,
+				s: newFogProps.s,
+				l: newFogProps.l,
+				ease: Power3.easeOut,
+				onUpdate: this.updateFogColor,
+				onUpdateScope: this
+			});
+		},
+
+		getNewFogProps: function () {
+			return {
+				h: Math.random(),
+				s: 0.25 + 0.25 * Math.random(),
+				l: 0.5 + 0.5 * Math.random()
+			}
+		},
+
+		updateFogColor: function () {
+			var fogColor = this.get('fogColor');
+			var fogProps = this.get('fogProps');
+			fogColor.setHSL(fogProps.h, fogProps.s, fogProps.l);
 		}
 	});
 
