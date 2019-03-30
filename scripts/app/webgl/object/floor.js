2a084acbec09fe08f8d3da77cfc89ff376839393
Thijs van Hoof <thijs.v.h@gmail.com>
2019-03-29 21:25:08 -0400

    floor invisible

--- scripts/app/webgl/object/floor.js
+++ scripts/app/webgl/object/floor.js
@@ -9,8 +9,9 @@ define([
 		initialize: function () {
 			var width = StateModel.get('width');
 			var depth = StateModel.get('depth');
+
 			this.geometry = new THREE.PlaneGeometry(width, depth);
-			this.material = new THREE.MeshBasicMaterial({ color: 0x004a2c });
+			this.material = new THREE.MeshBasicMaterial({ visible: false });
 			this.mesh = new THREE.Mesh(this.geometry, this.material);
 			this.mesh.position.set(0, 0, depth * -0.5);
 			this.mesh.rotation.set(Math.PI * 1.5, 0, 0);
