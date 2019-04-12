0d2aa458bd50200f780110df9760b815221cd462
Thijs van Hoof <thijs.v.h@gmail.com>
2019-04-11 22:18:03 -0400

    add effect composer and bokeh shader

--- scripts/app/webgl/core/renderer.js
+++ scripts/app/webgl/core/renderer.js
@@ -3,12 +3,21 @@ define([
 	'three',
 	'model/display-model',
 	'model/state-model',
-	'model/webgl-model'
+	'model/webgl-model',
+	'three-bokeh-pass',
+	'three-render-pass',
+	'three-shader-pass',
+	'three-effect-composer'
 ], function (WebGL, THREE, DisplayModel, StateModel, WebGLModel) {
 
 	return WebGL.extend({
 
 		initialize: function (options) {
+
+			var width = DisplayModel.get('width');
+			var height = DisplayModel.get('height');
+			var scene = WebGLModel.get('scene').scene;
+			var camera = WebGLModel.get('camera').camera;
 
 			this.renderer = new THREE.WebGLRenderer({
 				canvas: options.canvas,
@@ -16,13 +25,24 @@ define([
 				antialias: false
 			});
 
-			this.renderer.autoClear = true;
+			this.renderer.autoClear = false;
 			this.renderer.sortObjects = false;
 			this.renderer.setClearColor(StateModel.get('fogColor'), 1);
 			this.renderer.setPixelRatio(window.devicePixelRatio || 1);
+			this.renderer.setSize(width, height);
 
-			this.onResize();
+			this.composer = new THREE.EffectComposer(this.renderer);
+			this.composer.addPass(new THREE.RenderPass(scene, camera));
+			this.composer.addPass(new THREE.BokehPass(scene, camera, {
+				focus:		4750,
+				aperture:	0.00001,
+				maxblur:	1,
+				width:		width,
+				height:		height
+			}));
+
 			this.listenTo(DisplayModel, 'resize', this.onResize);
+			this.listenTo(WebGLModel, 'update', this.update);
 			this.listenTo(WebGLModel, 'render', this.render);
 			this.listenTo(StateModel, StateModel.UPDATE.FOG_COLOR, this.onUpdateFogColor);
 		},
@@ -31,10 +51,15 @@ define([
 			var width = DisplayModel.get('width'),
 				height = DisplayModel.get('height');
 			this.renderer.setSize(width, height);
+			this.composer.setSize(width, height);
 		},
 
-		render: function (scene, camera) {
-			this.renderer.render(scene, camera);
+		update: function (delta) {
+			this.delta = delta;
+		},
+
+		render: function () {
+			this.composer.render(this.delta);
 		},
 
 		onUpdateFogColor: function (fogColor) {
